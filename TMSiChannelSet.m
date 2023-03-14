ff8279c29bdf21fd8c18d53b96b100f64059619a
Max Murphy <max.murphy11@gmail.com>
2023-03-14 10:07:08 -0400

    Adds TMSiChannelSet enumeration
    For use in combination with +TMSiSAGA package.

---
+++ TMSiChannelSet.m
@@ -0,0 +1,12 @@
+classdef TMSiChannelSet < uint8
+    %TMSICHANNELSET  Enumeration class (uint8) for TMSiSAGA default channel-set types.
+    %
+    % Enumerated values:
+    %   NHP            (1)
+    %   NHP_EMG        (1)
+    
+    enumeration
+        NHP            (1)
+        NHP_EMG        (1)
+    end
+end
