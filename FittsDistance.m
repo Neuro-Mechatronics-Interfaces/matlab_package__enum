7b958c4c08455e9f0ec23f2860c59b47a5bf2ad3
Max (Lenovo) <max.murphy11@gmail.com>
2024-07-09 13:04:51 -0400

    Adds FittsDistance and FittsWidth enumerations from MWB tasks

---
+++ FittsDistance.m
@@ -0,0 +1,13 @@
+classdef FittsDistance < int32
+    %FITTSDISTANCE  Defines different target widths
+    
+    enumeration
+        UNKNOWN (-1)
+        D1 (6)
+        Near (6)
+        D2 (12)
+        Middle (12)
+        D3 (18)
+        Far (18)
+    end
+end
---
+++ FittsWidth.m
@@ -0,0 +1,16 @@
+classdef FittsWidth < int32
+    %FITTSWIDTH  Defines different target widths
+    
+    enumeration
+        UNKNOWN (-1)
+        W1 (1)
+        Small (1)
+        W2 (2)
+        Medium (2)
+        W3 (4)
+        Large (4)
+    end
+end
+
+
+
