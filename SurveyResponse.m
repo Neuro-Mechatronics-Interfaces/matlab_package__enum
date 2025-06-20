7181cdf63cdeb515a5aa6237007a1ccb017b8334
Max (Lenovo) <max.murphy11@gmail.com>
2025-06-20 06:55:11 -0400

    Adds SurveyResponse enumeration
    1 strongly disagree - 5 strongly agree

---
+++ SurveyResponse.m
@@ -0,0 +1,14 @@
+classdef SurveyResponse < double
+    %SURVEYRESPONSE Enumeration class for survey response agreement.
+    
+    enumeration
+        Empty            (-1)
+        StronglyDisagree (1)
+        Stronglydisagree (1)
+        Disagree         (2)
+        Neutral          (3)
+        Agree            (4)
+        StronglyAgree    (5)
+        Stronglyagree    (5)
+    end
+end
